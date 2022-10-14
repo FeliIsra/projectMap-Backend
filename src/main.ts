6a31ff05e8eebe9874ad1132d4616d7a877cc87c
FeliIsra <fisrael@est.frba.utn.edu.ar>
2022-10-13 23:33:02 -0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
