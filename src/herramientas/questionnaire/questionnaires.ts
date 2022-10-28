29f480545e2b76294b91430e79a7fd682acab1b0
FeliIsra <fisrael@est.frba.utn.edu.ar>
2022-10-28 19:23:58 -0300

    cambiar cuestionario

--- src/herramientas/questionnaire/questionnaires.ts
+++ src/herramientas/questionnaire/questionnaires.ts
@@ -1,50 +1,57 @@
 export class Questionnaires {
   static getChapters = {
     1: {
-      title: 'Tema 1',
-      presentations: ['ppt1', 'ppt2'],
+      title: '¿Cuál es la definición de digitalización?',
+      presentations: [
+        'La digitalización es el proceso de transformar procesos analógicos y objetos físicos en digitales. Considera cómo el escaneo de documentos de papel o el uso del almacenamiento en la nube para guardar todos tus archivos importantes eliminan la necesidad de archivadores anticuados. Todo aquello que antes hacíamos sin conexión y en papel ahora se volvió completamente digital gracias a la digitalización y tecnologías digitales mucho más eficaces. El término digitalización, a pesar de ser bastante transparente, se usa para describir el proceso general de volverse digital; es decir, mover el plan de marketing empresarial a un futuro más digital.',
+        'Existen muchas razones para volverse digital y la primera es que la mayoría de las personas ya han hecho el cambio. Quizás hayas notado que tu banco te ofrece la opción de recibir extractos sin usar papel o que la compañía de energía opta por usar facturas digitales. Definitivamente, puedes decidir seguir usando las formas antiguas, pero la sociedad, en general, está adoptando un futuro digitalizado, por lo que te convendría hacer lo mismo. Las marcas y las viviendas en todas partes están atravesando una transformación digital. Existen modelos empresariales completos que se basan en la estructura digital y por una buena razón. Aquí te mostramos algunos de los beneficios:',
+      ],
       questions: {
         1: {
-          question: 'Pregunta 1',
+          question:
+            '¿Conoces la importancia que la digitalización puede tener sobre tu negocio?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
-          },
-          right_answer: 2,
-        },
-        2: {
-          question: 'Pregunta 2',
-          answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
-          },
-          right_answer: 2,
-        },
-        3: {
-          question: 'Pregunta 3',
-          answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'No creo que aporte grandes beneficios al negocio',
+            2: 'Creo que para algunos negocios tiene sentido, pero no para la mayoria',
+            3: 'Es importante y agnostico al rubro',
           },
           right_answer: 3,
         },
-        4: {
-          question: 'Pregunta 4',
+        2: {
+          question: '¿Que es la digitalizacion?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'No usar mas papel en las empresas',
+            2: 'Una iniciativa de reciclaje',
+            3: 'El proceso de transformar procesos analógicos y objetos físicos en digitales',
+          },
+          right_answer: 3,
+        },
+        3: {
+          question: '¿Para que sirve digitalizar?',
+          answers: {
+            1: 'Funcionalmente para nada, pero esta bueno seguir los trends organizacionales',
+            2: 'Te permite organizar a tu empresa de una manera cuasi autonoma, resguardando el conocimiento en estructuras y no en personas',
+            3: 'Para resolver todos los problemas logisitcos de tu empresa',
+          },
+          right_answer: 2,
+        },
+        4: {
+          question: '¿Que beneficios tiene digitalizar?',
+          answers: {
+            1: 'Simplifica el intercambio de documentos, es más ecológico y rentable',
+            2: 'Simplifica el intercambio de documentos, es más ecológico y rentable, es más seguro',
+            3: 'Simplifica el intercambio de documentos, es más ecológico y rentable, es más seguro, fácil organización',
           },
           right_answer: 3,
         },
       },
     },
     2: {
-      title: 'Tema 2',
-      presentations: ['ppt1', 'ppt2'],
+      title: 'What is Lorem Ipsum?',
+      presentations: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      ],
       questions: {
         1: {
           question: 'Pregunta 1',
@@ -53,7 +60,7 @@ export class Questionnaires {
             2: 'Respuesta 2',
             3: 'Respuesta 3',
           },
-          right_answer: 2,
+          right_answer: 1,
         },
         2: {
           question: 'Pregunta 2',
