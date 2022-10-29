7397be0a17e9f241bfd1b93ad289cc7ee8ee7ce4
Bufa <julian@bluematador.com>
2022-10-29 13:55:19 -0300

    fix foda

--- src/herramientas/foda/foda.schema.ts
+++ src/herramientas/foda/foda.schema.ts
@@ -1,59 +1,53 @@
 import * as mongoose from 'mongoose';
 import { Area, Importancia, Intensidad, Tendencia, Urgencia } from './enums';
 import { Completition } from '../completition';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-export const FODASchema = new mongoose.Schema({
-  projectId: { type: String, require: true },
-  titulo: { type: String, require: true },
-  createdAt: { type: Date, default: Date.now() },
-  factores: [
-    {
-      area: {
-        type: String,
-        require: true,
-        enum: Area,
-      },
-      descripcion: {
-        type: String,
-        require: true,
-      },
-      importancia: {
-        type: String,
-        require: true,
-        enum: Importancia,
-      },
-      intensidad: {
-        type: String,
-        require: true,
-        enum: Intensidad,
-      },
-      tendencia: {
-        type: String,
-        require: true,
-        enum: Tendencia,
-      },
-      urgencia: {
-        type: String,
-        require: true,
-        enum: Urgencia,
-      },
-      puntuacion: {
-        type: Number,
-        require: true,
-        default: function () {
-          return 0;
-        },
-      },
-    },
-  ],
-  completion: {
-    type: String,
-    require: false,
-    default: function () {
-      return Completition.Vacio;
-    },
-  },
-});
+@Schema()
+export class Factor {
+  @Prop({ type: String, required: true })
+  descripcion: Area;
+
+  @Prop({ type: String, required: true })
+  area: Area;
+
+  @Prop({ type: String, required: true })
+  importancia: Importancia;
+
+  @Prop({ type: String, required: true })
+  intensidad: Intensidad;
+
+  @Prop({ type: String, required: true })
+  tendencia: Tendencia;
+
+  @Prop({ type: String, required: true })
+  urgencia: Urgencia;
+
+  @Prop({ type: Number, required: true, default: 0 })
+  puntuacion: number;
+}
+
+export const factorSchema = SchemaFactory.createForClass(Factor);
+
+@Schema()
+export class Foda {
+  @Prop({ required: true })
+  projectId: string;
+
+  @Prop({ type: String })
+  titulo: string;
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date;
+
+  @Prop([factorSchema])
+  factores: Factor[];
+
+  @Prop({ type: String, default: Completition.Vacio })
+  completion: Completition;
+}
+
+export const FODASchema = SchemaFactory.createForClass(Foda);
 
 FODASchema.pre('save', function (next) {
   const amenaza = this.factores.find(
