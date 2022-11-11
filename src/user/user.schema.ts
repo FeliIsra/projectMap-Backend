ed97babd197ad0d55fb7b2a547f6e8695cb7e6a7
Bufa <julian@bluematador.com>
2022-11-11 18:42:15 -0300

    unique email

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -15,7 +15,7 @@ export class User {
   @Prop({ type: String, required: true })
   lastName: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, unique: true })
   email: string;
 
   @Prop({ type: String, required: true })
