17b882c3b050f7c3addd3661c2195a2b585830d7
FeliIsra <fisrael@est.frba.utn.edu.ar>
2022-11-16 21:15:24 -0300

    agrego un cuestionario mas

--- src/herramientas/questionnaire/questionnaires.ts
+++ src/herramientas/questionnaire/questionnaires.ts
@@ -91,5 +91,50 @@ export class Questionnaires {
         },
       },
     },
+    3: {
+      title: 'What is Lorem Ipsum?',
+      presentations: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      ],
+      questions: {
+        1: {
+          question: 'Pregunta 1',
+          answers: {
+            1: 'Respuesta 1',
+            2: 'Respuesta 2',
+            3: 'Respuesta 3',
+          },
+          right_answer: 1,
+        },
+        2: {
+          question: 'Pregunta 2',
+          answers: {
+            1: 'Respuesta 1',
+            2: 'Respuesta 2',
+            3: 'Respuesta 3',
+          },
+          right_answer: 2,
+        },
+        3: {
+          question: 'Pregunta 3',
+          answers: {
+            1: 'Respuesta 1',
+            2: 'Respuesta 2',
+            3: 'Respuesta 3',
+          },
+          right_answer: 3,
+        },
+        4: {
+          question: 'Pregunta 4',
+          answers: {
+            1: 'Respuesta 1',
+            2: 'Respuesta 2',
+            3: 'Respuesta 3',
+          },
+          right_answer: 3,
+        },
+      },
+    },
   };
 }
