8b8b3dad9806cd95d1673b1c9205d446b57aed32
FeliIsra <fisrael@est.frba.utn.edu.ar>
2022-11-23 17:16:32 -0300

    probando el nuevo cuestionario

--- src/herramientas/questionnaire/questionnaires.ts
+++ src/herramientas/questionnaire/questionnaires.ts
@@ -3,8 +3,8 @@ export class Questionnaires {
     1: {
       title: '¿Cuál es la definición de digitalización?',
       presentations: [
-        'La digitalización es el proceso de transformar procesos analógicos y objetos físicos en digitales. Considera cómo el escaneo de documentos de papel o el uso del almacenamiento en la nube para guardar todos tus archivos importantes eliminan la necesidad de archivadores anticuados. Todo aquello que antes hacíamos sin conexión y en papel ahora se volvió completamente digital gracias a la digitalización y tecnologías digitales mucho más eficaces. El término digitalización, a pesar de ser bastante transparente, se usa para describir el proceso general de volverse digital; es decir, mover el plan de marketing empresarial a un futuro más digital.',
-        'Existen muchas razones para volverse digital y la primera es que la mayoría de las personas ya han hecho el cambio. Quizás hayas notado que tu banco te ofrece la opción de recibir extractos sin usar papel o que la compañía de energía opta por usar facturas digitales. Definitivamente, puedes decidir seguir usando las formas antiguas, pero la sociedad, en general, está adoptando un futuro digitalizado, por lo que te convendría hacer lo mismo. Las marcas y las viviendas en todas partes están atravesando una transformación digital. Existen modelos empresariales completos que se basan en la estructura digital y por una buena razón. Aquí te mostramos algunos de los beneficios:',
+        '<br>La <b>digitalización</b> es el proceso de transformar procesos analógicos y objetos físicos en digitales. Considera cómo el escaneo de documentos de papel o el uso del almacenamiento en la nube para guardar todos tus archivos importantes eliminan la necesidad de archivadores anticuados. Todo aquello que antes hacíamos sin conexión y en papel ahora se volvió completamente digital gracias a la digitalización y tecnologías digitales mucho más eficaces. El término digitalización, a pesar de ser bastante transparente, se usa para describir el proceso general de volverse digital; es decir, mover el plan de marketing empresarial a un futuro más digital.',
+        '<br>Existen muchas razones para volverse digital y la primera es que la mayoría de las personas ya han hecho el cambio. Quizás hayas notado que tu banco te ofrece la opción de recibir extractos sin usar papel o que la compañía de energía opta por usar facturas digitales. Definitivamente, puedes decidir seguir usando las formas antiguas, pero la sociedad, en general, está adoptando un futuro digitalizado, por lo que te convendría hacer lo mismo. Las marcas y las viviendas en todas partes están atravesando una transformación digital. Existen modelos empresariales completos que se basan en la estructura digital y por una buena razón. Aquí te mostramos algunos de los beneficios:',
       ],
       questions: {
         1: {
@@ -47,47 +47,48 @@ export class Questionnaires {
       },
     },
     2: {
-      title: 'What is Lorem Ipsum?',
+      title: 'Willie Nelson y la transformación corporativa?',
       presentations: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        '<br>Las organizaciones de todo el mundo se están viendo obligadas a lanzar iniciativas de transformación, no por gusto, sino para incrementar sus <b>probabilidades de supervivencia</b>. Estos procesos son traumáticos y van mucho más allá de tener un plan recogido en un par de cientos de diapositivas. Una transformación corporativa es una iniciativa que requiere alineamiento, un amplio espectro de capacidades poco habituales en las compañías, dedicación absoluta a la ejecución del plan y una plantilla de implementación que nos ayude a contestar “qué hacer” y “cuándo hacerlo”.<br><br>Las dudas más recurrentes de los clientes a la hora de abordar la ejecución de un proyecto de transformación corporativa giran en torno a la velocidad a la que deben poner en marcha los diferentes programas. Y, desde luego, hacen bien en preocuparse por acertar en esta decisión.',
+        '<br>Como en todas las cosas importantes, en este proceso dependemos de muchos factores que en su mayoría son poco <b>tangibles</b>, altamente <b>inestables</b> y que entran en juego según secuencias muy específicas, lo que hace que la elección del timing apropiado sea un ejercicio altamente incierto y con vigencia temporal muy estrecha.<br><br>El genial Willie Nelson capturó la relevancia del timing en la toma de decisiones importantes cuando dijo: <i>“el pájaro que madruga se queda con el gusano, pero el segundo ratón se queda con el queso”</i>. Aquí se refleja perfectamente el equilibrio entre la audacia y prudencia tan necesaria para sobrevivir.',
       ],
       questions: {
         1: {
-          question: 'Pregunta 1',
+          question: '¿A qué se refería Willie Nelson con su famoso dicho?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
-          },
-          right_answer: 1,
-        },
-        2: {
-          question: 'Pregunta 2',
-          answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'Se refería a la necesidad de ser lo más rápido posible en cambiar para asegurarse algo de ganancias.',
+            2: 'Se refería a la importancia de ser equilibrado a la hora de ser audaz y prudente en cambios estructurales de una organización.',
+            3: 'Se refiere a la necesidad de ser muy prudente a la hora de hacer cambios estructurales en su empresa.',
           },
           right_answer: 2,
         },
-        3: {
-          question: 'Pregunta 3',
+        2: {
+          question:
+            '¿Qué tipo de factores son los más comunes en los procesos de transformación?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'Factores intangibles y claros.',
+            2: 'Factores tangibles y claros.',
+            3: 'Factores intangibles e inciertos.',
           },
           right_answer: 3,
         },
-        4: {
-          question: 'Pregunta 4',
+        3: {
+          question: '¿Qué es cierto de las organizaciones del siglo 21?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'Deben necesariamente pasar por procesos de transformación.',
+            2: 'Pueden mantenerse estáticos y llevar igualmente una buena actividad económica.',
+            3: 'Pueden estar el 100% del tiempo ejecutando cambios sin problema.',
           },
-          right_answer: 3,
+          right_answer: 1,
+        },
+        4: {
+          question: '¿Que es cierto de un plan de transformación?',
+          answers: {
+            1: 'Es una guia específicamente creada para un momento específico de una empresa específica.',
+            2: 'Es un plan que puede usarse en cualquier empresa en cualquier momento.',
+            3: 'Es una receta de éxito en cualquier rubro, ya que cambiar implica mejorar.',
+          },
+          right_answer: 1,
         },
       },
     },
