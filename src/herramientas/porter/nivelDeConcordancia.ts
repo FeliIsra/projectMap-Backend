03dae4906dabdc82e326848c611c00b721b41cf1
Bufa <julian@bluematador.com>
2022-11-23 18:35:31 -0300

    fix typos porter

--- src/herramientas/porter/nivelDeConcordancia.ts
+++ src/herramientas/porter/nivelDeConcordancia.ts
@@ -3,5 +3,5 @@ export enum NivelDeConcordancia {
   CONCUERDO_EN_PARTE = 'Concuerdo parcialmente',
   NI_DE_ACUERDO_NI_EN_DESACUERDO = 'Indiferente',
   DE_ACUERDO_EN_PARTE = 'Desacuerdo parcialmente',
-  DE_ACUERDO_TOTALMENTE = 'Descuerdo complemente',
+  DE_ACUERDO_TOTALMENTE = 'Desacuerdo completamente',
 }
