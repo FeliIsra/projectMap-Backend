899880a75035d3940e196855e71a3f18f3ac5e87
FeliIsra <fisrael@est.frba.utn.edu.ar>
2022-11-23 17:43:54 -0300

    nuevo cuestionario

--- src/herramientas/questionnaire/questionnaires.ts
+++ src/herramientas/questionnaire/questionnaires.ts
@@ -47,7 +47,7 @@ export class Questionnaires {
       },
     },
     2: {
-      title: 'Willie Nelson y la transformación corporativa?',
+      title: 'Willie Nelson y la transformación corporativa',
       presentations: [
         '<br>Las organizaciones de todo el mundo se están viendo obligadas a lanzar iniciativas de transformación, no por gusto, sino para incrementar sus <b>probabilidades de supervivencia</b>. Estos procesos son traumáticos y van mucho más allá de tener un plan recogido en un par de cientos de diapositivas. Una transformación corporativa es una iniciativa que requiere alineamiento, un amplio espectro de capacidades poco habituales en las compañías, dedicación absoluta a la ejecución del plan y una plantilla de implementación que nos ayude a contestar “qué hacer” y “cuándo hacerlo”.<br><br>Las dudas más recurrentes de los clientes a la hora de abordar la ejecución de un proyecto de transformación corporativa giran en torno a la velocidad a la que deben poner en marcha los diferentes programas. Y, desde luego, hacen bien en preocuparse por acertar en esta decisión.',
         '<br>Como en todas las cosas importantes, en este proceso dependemos de muchos factores que en su mayoría son poco <b>tangibles</b>, altamente <b>inestables</b> y que entran en juego según secuencias muy específicas, lo que hace que la elección del timing apropiado sea un ejercicio altamente incierto y con vigencia temporal muy estrecha.<br><br>El genial Willie Nelson capturó la relevancia del timing en la toma de decisiones importantes cuando dijo: <i>“el pájaro que madruga se queda con el gusano, pero el segundo ratón se queda con el queso”</i>. Aquí se refleja perfectamente el equilibrio entre la audacia y prudencia tan necesaria para sobrevivir.',
@@ -93,45 +93,46 @@ export class Questionnaires {
       },
     },
     3: {
-      title: 'What is Lorem Ipsum?',
+      title: 'Entender qué tipo de mercado y organización tenemos',
       presentations: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        '<br>Entre todos estamos generando una sensación de que los sectores están disrumpiendo o se están transformando de manera acelerada. La realidad es siempre más compleja.<br><br>Cada industria está <b>evolucionando</b> de manera diferente debido a la importancia de las tecnologías de la información, de su cercanía con los consumidores y de la fragmentación del control de los activos claves para competir. Esto es: el sector minero o el agrícola no se encuentran bajo las mismas presiones que los productos de consumo.<br><br>Sin embargo, lo relevante es entender el nivel de <b>disrupción</b> que está teniendo lugar. No importa el número de novedades, sino cuántos elementos están cambiando las dinámicas competitivas establecidas, la profundidad que están teniendo y a la velocidad a la que todo está ocurriendo. La combinación de estos factores determinará el reto y las herramientas para resolverlo.<br><br>La distinta intensidad de esos factores puede permitir hacer un seguimiento de la evolución de sus mercados, como puede suceder en el caso de la minería. Pero también puede obligarlas a realizar rediseños corporativos a la desesperada, como hemos visto en los sectores de ocio y viajes.',
+        '<br>A pesar de que el modelo de <b>organización moderna</b> y exitosa pueda no ser único, las compañías existentes que compiten en las diferentes industrias son radicalmente diferentes, por lo que la jornada de transformación hacia el <i>role model</i> no puede ser compartida por todas.<br><br>Qué tipo de organización eres, en qué te quieres convertir y cómo de externalizadas están tus operaciones, son preguntas cuyas contestaciones te deben ayudar a decidir el camino y la velocidad a la que debes implementar la transformación.<br><br>Un <b>análisis</b> con mayor perspectiva nos hace entender que no podemos seguir creyendo que el mundo gira en el interior de los cristales corporativos. Te transformas para poder ser más eficiente en la entrega de valor a tus usuarios, pero en esa cadena de valor hay más organizaciones involucradas. Por lo tanto, es necesario incorporar al proceso también a nuestros proveedores.',
       ],
       questions: {
         1: {
-          question: 'Pregunta 1',
+          question: '¿Por qué evolucionan las industrias?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'Por las redes sociales y su influencia en la tecnología.',
+            2: 'Por la importancia de las tecnologías de la información, entre otras cosas.',
+            3: 'Por su elevada separación de los consumidores.',
           },
-          right_answer: 1,
+          right_answer: 2,
         },
         2: {
-          question: 'Pregunta 2',
+          question:
+            '¿Qué es lo que obliga a las corporaciones a hacer rediseños de sus estructuras?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'La combinación de los factores tecnológicos, consumidores y la fragmentación del control de las claves competitivas.',
+            2: 'Sus integrantes y sus deseos.',
+            3: 'Los stakeholders inversores de las compañías.',
           },
           right_answer: 2,
         },
         3: {
-          question: 'Pregunta 3',
+          question: '¿Que es cierto de las organizaciones según el texto?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'Las organizaciones más exitosas son todas idénticas en estructura.',
+            2: 'Las organizaciones exitosas son exitosas porque tienen una estructura acorde a su rubro.',
+            3: 'Las organizaciones son exitosas por la velocidad en la que implementan sus cambios.',
           },
           right_answer: 3,
         },
         4: {
-          question: 'Pregunta 4',
+          question: '¿Por qué se transforman las empresas?',
           answers: {
-            1: 'Respuesta 1',
-            2: 'Respuesta 2',
-            3: 'Respuesta 3',
+            1: 'Para ser más eficiente y entregar más valor a tus usuarios.',
+            2: 'Para dar una mejor imagen ante sus usuarios.',
+            3: 'Para dar una mejor imagen ante sus inversores.',
           },
           right_answer: 3,
         },
